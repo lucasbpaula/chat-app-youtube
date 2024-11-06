264021bb89423a78b45a72ef0131f4b81cf32554
lbpaula <lbpaula@brasilseg.com.br>
2024-11-06 20:27:49 -0300

    feat(backend): adds sendMessage and protect route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,8 +13,10 @@ dotenv.config();
 
 // middlewares
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser()); // to access the cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
