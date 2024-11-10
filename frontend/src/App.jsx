3eab93f69619914964903abd48cc22e5b7f41b7d
lbpaula <lbpaula@brasilseg.com.br>
2024-11-10 19:42:42 -0300

    chore(frontend): update tsx files to jsx and adds tailwind utilities in css index

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/SignUp'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center p-4">
+      <Home />
+    </div>
+  )
+}
+
+export default App
